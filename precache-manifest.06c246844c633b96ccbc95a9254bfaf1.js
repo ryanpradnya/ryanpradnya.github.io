024233925d5118dec2bb4263a415d85828984730
ryanpradnya <ryanpradnya@gmail.com>
2019-10-27 23:30:50 +0700

    Initial commit

---
+++ precache-manifest.06c246844c633b96ccbc95a9254bfaf1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bca12d03c8214dcf8fed90e41f20ab2",
+    "url": "/myportofolio/index.html"
+  },
+  {
+    "revision": "b7631f9a8e37f50ef4cc",
+    "url": "/myportofolio/static/css/2.6a0fa25c.chunk.css"
+  },
+  {
+    "revision": "5363311de71471dc0685",
+    "url": "/myportofolio/static/css/main.e5721cd2.chunk.css"
+  },
+  {
+    "revision": "b7631f9a8e37f50ef4cc",
+    "url": "/myportofolio/static/js/2.31d96d61.chunk.js"
+  },
+  {
+    "revision": "5363311de71471dc0685",
+    "url": "/myportofolio/static/js/main.9e19dba2.chunk.js"
+  },
+  {
+    "revision": "a5a400b496a86ade1b62",
+    "url": "/myportofolio/static/js/runtime-main.694b17d2.js"
+  },
+  {
+    "revision": "c0f1e4a4fdfb8048c72e86aadb2a247d",
+    "url": "/myportofolio/static/media/Roboto-Bold.c0f1e4a4.woff2"
+  },
+  {
+    "revision": "eed9aab5449cc9c8430d7d258108f602",
+    "url": "/myportofolio/static/media/Roboto-Bold.eed9aab5.woff"
+  },
+  {
+    "revision": "3c37aa69cd77e6a53a067170fa8fe2e9",
+    "url": "/myportofolio/static/media/Roboto-Light.3c37aa69.woff2"
+  },
+  {
+    "revision": "ea36cd9a0e9eee97012a67b8a4570d7b",
+    "url": "/myportofolio/static/media/Roboto-Light.ea36cd9a.woff"
+  },
+  {
+    "revision": "1561b424aaef2f704bbd89155b3ce514",
+    "url": "/myportofolio/static/media/Roboto-Medium.1561b424.woff2"
+  },
+  {
+    "revision": "cf4d60bc0b1d4b2314085919a00e1724",
+    "url": "/myportofolio/static/media/Roboto-Medium.cf4d60bc.woff"
+  },
+  {
+    "revision": "3cf6adf61054c328b1b0ddcd8f9ce24d",
+    "url": "/myportofolio/static/media/Roboto-Regular.3cf6adf6.woff"
+  },
+  {
+    "revision": "5136cbe62a63604402f2fedb97f246f8",
+    "url": "/myportofolio/static/media/Roboto-Regular.5136cbe6.woff2"
+  },
+  {
+    "revision": "1f35e6a11d27d2e10d28946d42332dc5",
+    "url": "/myportofolio/static/media/Roboto-Thin.1f35e6a1.woff2"
+  },
+  {
+    "revision": "44b78f142603eb69f593ed4002ed7a4a",
+    "url": "/myportofolio/static/media/Roboto-Thin.44b78f14.woff"
+  },
+  {
+    "revision": "859e9af7840da8e0dcbc72a8667c74a8",
+    "url": "/myportofolio/static/media/avatar.859e9af7.jpg"
+  },
+  {
+    "revision": "64156da2c06ea06244889ea4bb01259e",
+    "url": "/myportofolio/static/media/bg-skill.64156da2.jpg"
+  },
+  {
+    "revision": "15fb81c23e7969ff61f6a2b6cfba9a43",
+    "url": "/myportofolio/static/media/blue-orange.15fb81c2.jpg"
+  },
+  {
+    "revision": "8345689744c365bd975513ff6af0738b",
+    "url": "/myportofolio/static/media/parallax1.83456897.jpg"
+  },
+  {
+    "revision": "5069647f73eedf24652198ca0bfd08c4",
+    "url": "/myportofolio/static/media/parallax2.5069647f.jpg"
+  }
+]);
